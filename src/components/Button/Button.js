7f26b4c45083860571d2fa3b9da318fb6e3f6e08
Alexander Telyatnikov <labriko@yandex.ru>
2017-04-05 18:36:02 +0300

    Input

---
+++ src/components/Button/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const buttonStyles = {
+  border: '1px solid #eee',
+  borderRadius: 3,
+  backgroundColor: '#FFFFFF',
+  cursor: 'pointer',
+  fontSize: 15,
+  padding: '3px 10px',
+};
+
+const Button = ({ children, onClick, style = {} }) => ( //eslint-disable-line
+  <div>
+    <button
+      style={{ ...buttonStyles, ...style }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  </div>
+);
+
+Button.propTypes = {
+  children: React.PropTypes.string.isRequired,
+  onClick: React.PropTypes.func,
+  style: React.PropTypes.object,
+};
+
+export default Button;
---
+++ src/components/Input/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Input extends React.Component { // eslint-disable-line
+  render() {
+    return (
+      <input value="Test" />
+    );
+  }
+}
+
+export default Input;
